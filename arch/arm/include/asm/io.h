8a2f6ebefd94d17821fa47bb87e43966540e57a0
coolcpu <sghigh@yahoo.com>
2012-10-06 15:44:51 +0900

    enable ION memory manager & backport from android_kernel_common 3.4

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -81,6 +81,11 @@ extern void __iomem *__arm_ioremap_caller(unsigned long, size_t, unsigned int,
 extern void __iomem *__arm_ioremap_pfn(unsigned long, unsigned long, size_t, unsigned int);
 extern void __iomem *__arm_ioremap(unsigned long, size_t, unsigned int);
 extern void __iounmap(volatile void __iomem *addr);
+extern void __arm_iounmap(volatile void __iomem *addr);
+
+extern void __iomem * (*arch_ioremap_caller)(unsigned long, size_t,
+	unsigned int, void *);
+extern void (*arch_iounmap)(volatile void __iomem *);
 
 /*
  * Bad read/write accesses...
@@ -251,6 +256,7 @@ extern void _memset_io(volatile void __iomem *, int, size_t);
 #define ioremap_cached(cookie,size)	__arch_ioremap((cookie), (size), MT_DEVICE_CACHED)
 #define ioremap_wc(cookie,size)		__arch_ioremap((cookie), (size), MT_DEVICE_WC)
 #define iounmap				__arch_iounmap
+#define __arm_iounmap			__arch_iounmap
 
 /*
  * io{read,write}{8,16,32} macros
